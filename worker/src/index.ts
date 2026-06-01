90304bd72aae1d41226a81a18304e17cb86db908
whoeven99 <whoeven99@outlook.com>
2026-06-02 02:49:06 +0800

    update

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,8 @@
+import { ensureWorkerEnv } from "./env.js";
 import { startScheduler } from "./scheduler.js";
+
+// 最早执行：加载 Render Secret File + 诊断
+ensureWorkerEnv();
 
 console.log("[worker] spark-translation-worker starting");
 console.log(`[worker] PID=${process.pid} Node=${process.version}`);
