96cf4e1ecacf7ab761744709be8bc4a0a7f2707f
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-07-16 15:18:17 +0800

    1，修改import
    1，修改import

--- app/routes/app.settings.ads-insights.tsx
+++ app/routes/app.settings.ads-insights.tsx
@@ -2,6 +2,11 @@ import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
 import {
+  getGoogleAdsCredential,
+  getGoogleAdsSandboxCredential,
+  getGoogleAdsSandboxPending,
+  getMetaAdsCredential,
+  getMetaAdsPending,
   getTiktokAdsInsightsCredential,
   getTiktokCatalogCredential,
 } from "../server/adsCatalog/credentialStore.server";
